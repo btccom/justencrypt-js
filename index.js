1f24267af8518e8b568a05f4d804ff74af93cfda
Ruben de Vries <ruben@rubensayshi.com>
2017-01-13 10:43:23 +0100

    wip

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ module.exports = {
     KeyDerivation: require('./lib/keyderivation'),
     KeyDerivationAsync: require('./lib/keyderivation_async'),
     Encryption: require('./lib/encryption'),
-    EncryptionAsync: require('./lib/encryption_async'),
     EncryptionMnemonic: require('./lib/encryption_mnemonic')
 };
--- test.js
+++ test.js
@@ -1,8 +1,7 @@
 exports = module.exports = {
-    justencrypt: require('./'),
+    justencrypt: require('./'), // for debugging purposes
     keyderivation: require('./test/keyderivation.test'),
     keyderivation_async: require('./test/keyderivation_async.test'),
     encryption: require('./test/encryption.test'),
-    encryption_async: require('./test/encryption_async.test'),
     mnemonic: require('./test/mnemonic.test')
 };
