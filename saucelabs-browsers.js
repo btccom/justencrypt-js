213900d68aae329744c378f489a07a9d5a7b7523
Ruben de Vries <ruben@rubensayshi.com>
2017-01-19 15:20:27 +0100

    split browsers in 2 groups; with webcrypto and without webcrypto, test differently

--- saucelabs-browsers.js
+++ saucelabs-browsers.js
@@ -2,58 +2,70 @@ module.exports = [{
     // latest chrome
     browserName: 'googlechrome',
     platform: 'Windows 10',
-    version: 'latest'
+    version: 'latest',
+    webcrypto: true
 }, {
     // latest chrome as of writing
     browserName: 'googlechrome',
     platform: 'Windows 10',
-    version: '54.0'
+    version: '54.0',
+    webcrypto: true
 }, {
     // latest firefox
     browserName: 'firefox',
     platform: 'Windows 10',
-    version: 'latest'
+    version: 'latest',
+    webcrypto: true
 }, {
     // latest firefox as of writing
     browserName: 'firefox',
     platform: 'Windows 10',
-    version: '49.0'
+    version: '49.0',
+    webcrypto: true
 }, {
     // latest Safari
     browserName: 'Safari',
-    version: 'latest'
+    version: 'latest',
+    webcrypto: true
 }, {
     // latest Safari as of writing
     browserName: 'Safari',
-    version: '10.0'
+    version: '10.0',
+    webcrypto: true
 }, {
     // latest edge
     browserName: 'MicrosoftEdge',
     platform: 'Windows 10',
-    version: 'latest'
+    version: 'latest',
+    webcrypto: false
 }, {
     // latest edge as of writing
     browserName: 'MicrosoftEdge',
     platform: 'Windows 10',
-    version: '14.14393'
+    version: '14.14393',
+    webcrypto: false
 }, {
     // latest IE (EOL)
     browserName: 'internet explorer',
     platform: 'Windows 10',
-    version: '11.103'
+    version: '11.103',
+    webcrypto: false
 }, {
     // android 5.0
     browserName: 'android',
     platform: 'Linux',
-    version: '5.0'
+    version: '5.0',
+    webcrypto: false
 }, {
     // iphone iOS 9.2
     browserName: 'iphone',
     platform: 'OS X 10.10',
-    version: '9.2'
+    version: '9.2',
+    webcrypto: false
 }, {
     // on FF < 48 there's no crypto.getRandomValues in webworkers
     browserName: 'firefox',
     platform: 'Windows 10',
-    version: '47'
+    version: '47',
+    webcrypto: false
 }];
---
+++ test/testconfig.js
@@ -0,0 +1,5 @@
+var config = {
+    webcrypto: true
+};
+
+module.exports = config;
